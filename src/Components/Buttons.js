21dd3de356d80d063c41f3e7c9c6ac4504b723c6
Ian F <ian.ferg36@gmail.com>
2022-05-01 17:52:35 -0600

    added icons to buttons and made their links functional

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,10 +1,24 @@
 import React from "react";
+import EmailIcon from "../Images/Icon.jpg";
+import LinkedInIcon from "../Images/Vector.png";
 
 export default function Email() {
+  function emailButton() {
+    window.location = "mailto:ian.ferg36@gmail.com";
+  }
+  function linkedinButton() {
+    window.open("https://www.linkedin.com/in/ian-ferguson-827877153/");
+  }
+
   return (
     <div className="button-container">
-      <button className="button-email">Email</button>
-      <button className="button-linkedin">LinkedIn</button>
+      <button onClick={emailButton} className="button-email">
+        <img className="email-icon" src={EmailIcon} alt="envelope icon" /> Email
+      </button>
+      <button onClick={linkedinButton} className="button-linkedin">
+        <img className="linkedin-icon" src={LinkedInIcon} alt="linkedin icon" />
+        LinkedIn
+      </button>
     </div>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,10 +2,19 @@ import React from "react";
 import GitIcon from "../Images/GitHub Icon.png";
 
 export default function Footer() {
+  function gitButton() {
+    window.open("https://github.com/IanFerguson-36");
+  }
+
   return (
     <div>
       <footer className="footer">
-        <img className="git-icon" src={GitIcon} alt="github icon" />
+        <img
+          onClick={gitButton}
+          className="git-icon"
+          src={GitIcon}
+          alt="github icon"
+        />
       </footer>
     </div>
   );
